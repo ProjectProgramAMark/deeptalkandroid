aca5143c57f4eea8f5c6e18bc570573b5defa28e
ProjectProgramAMark <projectprogramamark@gmail.com>
2017-02-18 02:25:59 -0500

    added register activity functionality and choice activity

--- app/src/main/java/markmoussa/me/deeptalkandroid/MainActivity.java
+++ app/src/main/java/markmoussa/me/deeptalkandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package markmoussa.me.deeptalkandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,20 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Setting variables
-        final Button loginButton = (Button) findViewById(R.id.loginButton);
-        final Button registerButton = (Button) findViewById(R.id.registerButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Check login here
-                // If yes -> new screen
-            }
-        });
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Perform action to click
-                // Go to Register view
+                // Do some shit
+                Intent switchToRegisterActivity = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(switchToRegisterActivity);
             }
         });
     }
