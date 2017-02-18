24793c17e3f05baf09092fd083b6802f360ae475
ProjectProgramAMark <projectprogramamark@gmail.com>
2017-02-18 18:20:43 -0500

    started implementing volley pt. 1

--- app/src/main/java/markmoussa/me/deeptalkandroid/RegisterActivity.java
+++ app/src/main/java/markmoussa/me/deeptalkandroid/RegisterActivity.java
@@ -1,10 +1,18 @@
 package markmoussa.me.deeptalkandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -22,11 +30,32 @@ public class RegisterActivity extends AppCompatActivity {
         String phone = phoneEditText.getText().toString();
         String email = emailEditText.getText().toString();
 
+
+        // Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url ="http://localhost:6001/register";
+
+
         Button registerButton = (Button) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Do some shit here
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Send Bundles with this Intent so it has account information
+                                Intent goToChoiceActivity = new Intent(RegisterActivity.this, ChoiceActivity.class);
+                                startActivity(goToChoiceActivity);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
 
             }
         });
