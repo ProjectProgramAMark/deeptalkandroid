f96702dbf0cad2be13b88930eeafa4ad116ce65b
ProjectProgramAMark <projectprogramamark@gmail.com>
2017-02-18 01:29:48 -0500

    initial commit ayy lmao

--- app/src/main/java/markmoussa/me/deeptalkandroid/MainActivity.java
+++ app/src/main/java/markmoussa/me/deeptalkandroid/MainActivity.java
@@ -2,6 +2,8 @@ package markmoussa.me.deeptalkandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setting variables
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final Button registerButton = (Button) findViewById(R.id.registerButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Check login here
+                // If yes -> new screen
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Perform action to click
+                // Go to Register view
+            }
+        });
     }
 }
