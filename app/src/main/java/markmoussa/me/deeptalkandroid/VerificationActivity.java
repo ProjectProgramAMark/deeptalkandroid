058683db132421c11a72a942aa446e0993a1e475
ProjectProgramAMark <projectprogramamark@gmail.com>
2017-02-18 03:28:19 -0500

    fixed typo

--- app/src/main/java/markmoussa/me/deeptalkandroid/VerificationActivity.java
+++ app/src/main/java/markmoussa/me/deeptalkandroid/VerificationActivity.java
@@ -3,11 +3,11 @@ package markmoussa.me.deeptalkandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class VerificatinActivity extends AppCompatActivity {
+public class VerificationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verificatin);
+        setContentView(R.layout.activity_verification);
     }
 }
