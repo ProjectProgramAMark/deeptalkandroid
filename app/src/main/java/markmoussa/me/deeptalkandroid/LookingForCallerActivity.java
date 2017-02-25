58a4ecb4e0ecebec2e3886e5ab94ef863d9d0685
ezulo <ezuloaga0@gmail.com>
2017-02-25 11:48:24 -0500

    Added twilio stuff to LookingForCaller; added Twilio dependency on gradle file

--- app/src/main/java/markmoussa/me/deeptalkandroid/LookingForCallerActivity.java
+++ app/src/main/java/markmoussa/me/deeptalkandroid/LookingForCallerActivity.java
@@ -2,6 +2,7 @@ package markmoussa.me.deeptalkandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twilio.*;
 
 public class LookingForCallerActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class LookingForCallerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_looking_for_caller);
+
+        String api_key = "deeptalk";
+        String sid = "SK1f440def367d72042ce6f757b630fc25";
+        String secret = "HhnsKHr9Om5XBgsxx2nVpXmC0NQWCrmJ";
+
+
+
+        String phone_number = ""; //how do we get this?
+
     }
 }
